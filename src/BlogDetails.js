0dda747df4d3bd3265c44bcf90cb806be191a3df
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2024-08-27 01:50:40 +0300

    feat: Delete Blogs and programatic Redirects -end of course

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,26 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 
 const BlogDetails = () => {
     const { id } =useParams()
     const{data :blog,isPending,error} = useFetch('http://localhost:8000/blogs/' + id);
+   
+    const history = useHistory();
+
+    const handleClick = ()=>{
+        fetch('http://localhost:8000/blogs/' + blog.id,{
+            method: 'DELETE',
+          
+        }).then(()=>{
+            console.log("deleted")
+            // setIsPending(false)
+            // history.go(-1) means to go backward and + is farward 
+            history.push("/");
+        })
+    };
+
      return ( 
         <div className="blog-details">
           {isPending && <div>Loading . . . </div>}
@@ -14,6 +30,7 @@ const BlogDetails = () => {
                 <h2>{ blog.title }</h2>
                 <p>Written by {blog.author}</p>
                 <div>{blog.body}</div>
+                <button onClick={handleClick}>Delete</button>
             </article>
           )}
         </div>
