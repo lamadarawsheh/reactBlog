ec6be4265c0378beac222125efb57ff7ba02ad4d
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2024-08-26 20:54:50 +0300

    feat: add custom hook -lesson 20

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,12 @@
 
 import { useState,useEffect } from "react";import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 ;
 
 const Home = () => {
 
-    const [blogs,setBlogs] = useState(null);
-    const [isPending,setIsPending] = useState(true);
-    const [error,setError] = useState(null)
+  const { data :blogs , isPending , error } = useFetch("http://localhost:8000/blogs");
 
-
-
- 
-
-    //this func runs for every render ||| DON'T  CHNAGE STATE INSIDE IT IT WILL DO INFINITE LOOP
-    // I ADDED A DEPENDENCY ARRAY that make the func only run when the states that inside the dependency array changes BUT it alwyas runs on the first render 
-    useEffect(() => {
-     setTimeout(() => {
-        fetch("http://localhost:8000/blogs")
-        .then(
-            res =>{
-                console.log(res);
-                if(!res.ok){
-                    throw Error("couldn't fetch the data !!");
-                }
-                return res.json();
-            })
-        .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(error => {
-            setError(error.message);
-            setIsPending(false);
-        })
-     }, 1000);
-    },[]);
 
     return ( 
         <div className="home">  
