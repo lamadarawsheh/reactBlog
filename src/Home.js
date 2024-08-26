9783d702dd21d5efa689a1cf125f4ffca8aaddfd
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2024-08-26 16:51:46 +0300

    feat: fetch from jsoon server -this is up to lesson 19

--- src/Home.js
+++ src/Home.js
@@ -4,29 +4,44 @@ import { useState,useEffect } from "react";import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs,setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ]);
+    const [blogs,setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null)
 
 
 
-    //i created the handledelete func here cuz i want the change to refelct immediatly wich means to use the useState!!
-    const handleDelete = (id)=>{
-        const newBlogs = blogs.filter((blog)=>blog.id !== id);
-        setBlogs(newBlogs);
-    }
+ 
 
     //this func runs for every render ||| DON'T  CHNAGE STATE INSIDE IT IT WILL DO INFINITE LOOP
     // I ADDED A DEPENDENCY ARRAY that make the func only run when the states that inside the dependency array changes BUT it alwyas runs on the first render 
     useEffect(() => {
-        console.log("useeffect ran")
+     setTimeout(() => {
+        fetch("http://localhost:8000/blogs")
+        .then(
+            res =>{
+                console.log(res);
+                if(!res.ok){
+                    throw Error("couldn't fetch the data !!");
+                }
+                return res.json();
+            })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(error => {
+            setError(error.message);
+            setIsPending(false);
+        })
+     }, 1000);
     },[]);
 
     return ( 
         <div className="home">  
-        <BlogList blogs={blogs} title="All Blogs!" handleDelete = {handleDelete}/>
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading. . . . </div>}
+        {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         {/* <BlogList blogs={blogs.filter((blog)=>blog.author == "yoshi")} title="Mario's Blogs!"/> */}
         </div>
      );
